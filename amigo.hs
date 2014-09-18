525517da48e3d25d7f2bec3cef10d9a5fb01fe8f
Leonel Quinteros <leonel.quinteros@gmail.com>
2014-09-18 14:09:43 -0300

    Proof of concept

--- amigo.hs
+++ amigo.hs
@@ -6,6 +6,8 @@
 import Network
 import System.IO
 import Text.Printf
+import Data.List
+import System.Exit
 
 --
 -- Config section. To be replaced by a config file and learning abilities.
@@ -21,31 +23,71 @@ nick   = "peiiionbot"
 main = do
     irc <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering irc NoBuffering
-    write irc "NICK" nick
-    write irc "USER" (nick++" 0 * :Amigo")
-    write irc "JOIN" chan
-    listen irc
+    writeTo irc "NICK" nick
+    writeTo irc "USER" (nick++" 0 * :Amigo Bot")
+    writeTo irc "JOIN" chan
+    listenTo irc
 
 
 --
 -- Writes anything to the IRC socket.
 -- Takes the command and the params separately, for better formatting.
 --
-write :: Handle -> String -> String -> IO ()
-write irc command params = do
+writeTo :: Handle -> String -> String -> IO ()
+writeTo irc command params = do
     hPrintf irc "%s %s\r\n" command params
     printf    "> %s %s\n" command params
 
 
 --
--- Main listener loop.
+-- Main listenToer loop.
 -- Reads over the stream and send everything to the message processor.
+-- Handles PING/PONG by itself
 --
-listen :: Handle -> IO ()
-listen irc = forever $ do
-    s <- hGetLine irc
+listenTo :: Handle -> IO ()
+listenTo irc = forever $ do
+    msg <- hGetLine irc
+    let s = init msg
+    if ping s then pong s else processMsg irc (clean s)
     putStrLn s
   where
-    forever this = do
-        this
-        forever this
+    forever this = this >> forever this
+
+    clean   = drop 1 . dropWhile (/= ':') . drop 1
+
+    ping x  = isPrefixOf "PING :" x
+    pong x  = writeTo irc "PONG" (':' : drop 6 x)
+
+
+--
+-- IRC message processor.
+-- All IRC messages will go through this to know what to do with them.
+-- Checks for commands sent using '@Amigo' prefix.
+-- Then tries to talk.
+--
+processMsg :: Handle -> String -> IO ()
+processMsg irc msg
+    | isPrefixOf "@Amigo " msg  = runCommand irc (drop 7 msg)
+    | otherwise                 = talk irc msg
+
+
+--
+-- Amigo command protocol handler.
+--
+runCommand :: Handle -> String -> IO()
+runCommand irc msg
+    | isPrefixOf "go away!" msg = writeTo irc "QUIT" ":Exiting" >> exitWith ExitSuccess
+    | isPrefixOf "echo:" msg    = sendMsg irc (drop 5 msg)
+
+--
+-- Amigo tries to talk here. Not much yet.
+--
+talk :: Handle -> String -> IO()
+talk irc msg = return () -- ignore everything
+
+
+--
+-- IRC message sender wrapper.
+--
+sendMsg :: Handle -> String -> IO ()
+sendMsg irc msg = writeTo irc "PRIVMSG" (chan ++ " :" ++ msg)
