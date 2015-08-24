e495f6e92178d0d464528b1a4951ee967466ef21
Leonel Quinteros <leonel.quinteros@gmail.com>
2015-08-24 13:05:37 -0300

    Updated main comment docs

--- main.go
+++ main.go
@@ -3,12 +3,12 @@
 //
 // Command format has to be in the following form to be recognized:
 //
-//  NICK [ SPACE... ] PROTOCOL_COMMAND [ SPACE... ] 'param' [ SPACE PARAM_DELIMITER 'extra params' ... ]
+//  [ NICK ] [ COMMAND ] [ PARAM ] [ SPACE + PARAM_DELIMITER [ PARAM ] ... ]
 //
 //  Where NICK is the bot's nick on the IRC server,
-//  PROTOCOL_COMMAND is a string present in the protocol variable defined on this file,
-//  'param' and 'extra params' are any string formed by any chars other than a combination of SPACE and PARAM_DELIMITER,
-//  SPACE is the space character and PARAM_DELIMITER is defined in param_delimiter variable on this file, usually a double semicolon ';;'.
+//  COMMAND is a string present in the protocol variable defined on this file,
+//  [ PARAM ] is any string formed by any chars other than a combination of SPACE and PARAM_DELIMITER,
+//  SPACE is the space character and PARAM_DELIMITER is defined in param_delimiter constant on command.go, a double semicolon ";;" by default.
 //
 //  If PARAM_DELIMITER has to be included as part of a param value (usually not needed), you can escape it using a backslash \,
 //  Any combination of backslash and the param delimiter will be replaced by the param delimiter itself at parsing time.
